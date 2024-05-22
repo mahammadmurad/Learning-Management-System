0929e78df51e73c9ebe7d683df54f36f06ef5456
mahammadmurad <mahammad.muradd@gmail.com>
2024-05-22 19:29:33 +0400

    custom User and Profile models created

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from userauths.models import User, Profile
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'full_name', 'date']
+    
+
+admin.site.register(User)
 
 # Register your models here.
